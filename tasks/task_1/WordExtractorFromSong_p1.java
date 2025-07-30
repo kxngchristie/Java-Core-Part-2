292a3dc621846c222ad4e797a7e81e37fb53098c
kxngchristie <kchristie@griddynamics.com>
2025-07-30 14:51:45 -0500

    Refactor imports in WordExtractorFromSong_p1.java for clarity and organization

--- tasks/task_1/WordExtractorFromSong_p1.java
+++ tasks/task_1/WordExtractorFromSong_p1.java
@@ -1,7 +1,11 @@
 package task_1;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class WordExtractorFromSong_p1 {
         public static String[] getWordsFromSongLyrics(String lyrics) {
